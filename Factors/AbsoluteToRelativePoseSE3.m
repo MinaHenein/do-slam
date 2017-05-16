ba6843bbbefb8d8c0f8821fe3f5c12709de34aef
u5388842 <u5388842@anu.edu.au>
2017-05-16 11:51:30 +1000

    Working on Toy test for solver

---
+++ Factors/AbsoluteToRelativePoseSE3.m
@@ -0,0 +1,24 @@
+function posesRelative = AbsoluteToRelativePose(posesAbsolute,posesNewFrame)
+%% converts poses from absolute coords to new reference frame
+% inputs
+    %posesNewFrame = matrix of pose column vectors of new reference frame
+    %posesAbsolute = matrix of absolute pose column vectors, pose = [position; axis]
+% output
+    %posesRelative = matrix of relative pose column vectors, pose = [position; axis]
+
+%to store relative pose vectors
+posesRelative = zeros(size(posesAbsolute));  
+%VECTORISE THIS
+for i = 1:size(posesAbsolute,2)
+    screwAbsolute = [rot(posesAbsolute(4:6,i)) posesAbsolute(1:3,i);
+                     0 0 0 1];
+    screwNewFrame = [rot(posesNewFrame(4:6,i)) posesNewFrame(1:3,i);
+                     0 0 0 1];
+    screwRelative =  screwAbsolute \ screwNewFrame;
+    posesRelative(:,i) = [screwRelative(1:3,4); arot(screwRelative(1:3,1:3))];
+end
+
+end
+
+
+
